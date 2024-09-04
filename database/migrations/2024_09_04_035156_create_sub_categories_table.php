e5929497aeee022c9ae7bc1652b31468fa33f9ed
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-09-04 11:12:09 +0530

    Update 2024_09_04_035156_create_sub_categories_table.php

--- database/migrations/2024_09_04_035156_create_sub_categories_table.php
+++ database/migrations/2024_09_04_035156_create_sub_categories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('sub_categories', function (Blueprint $table) {
             $table->id();
-            $table->usignedBigInteger('category_id');
+            $table->unsignedBigInteger('category_id');
             $table->string('name');
             $table->timestamps();
         });
