69d9dcd59dd11a22e1c003ca5aee89d22f855ec5
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-09-04 17:37:15 +0530

    Update SubCategorySeeder.php

--- database/seeders/SubCategorySeeder.php
+++ database/seeders/SubCategorySeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\SubCategory;
+
 class SubCategorySeeder extends Seeder
 {
     /**
