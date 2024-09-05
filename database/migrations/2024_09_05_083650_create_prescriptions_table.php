7b86cf0014420b00e9845af0a8bb8e25c2db3fa8
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-09-05 14:39:46 +0530

    update and add enum file in Prescription migration

--- database/migrations/2024_09_05_083650_create_prescriptions_table.php
+++ database/migrations/2024_09_05_083650_create_prescriptions_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use App\Enums\PrescriptionStatus;
+
 return new class extends Migration
 {
     /**
@@ -16,7 +18,7 @@ return new class extends Migration
             $table->unsignedBigInteger('customer_id');  // Reference to the user/customer
             $table->string('file_path')->nullable();  // Path to the uploaded prescription file
             $table->text('notes')->nullable();  // Optional notes added by customer
-            $table->string('status')->default(); //['pending', 'reviewed', 'approved', 'rejected'])->default('pending');
+            $table->string('status')->default(PrescriptionStatus::Pending);
             $table->timestamps();
         });
     }
