5b33d81be0816931f8516770f27763b6777a2585
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-09-05 14:42:17 +0530

    create filament resource for Prescription model

---
+++ app/Filament/Resources/PrescriptionResource.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PrescriptionResource\Pages;
+use App\Filament\Resources\PrescriptionResource\RelationManagers;
+use App\Models\Prescription;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PrescriptionResource extends Resource
+{
+    protected static ?string $model = Prescription::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('customer_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\FileUpload::make('file_path')
+                    ->image()
+                    ->default(null),
+                Forms\Components\Textarea::make('notes')
+                    ->columnSpanFull(),
+                Forms\Components\Select::make('status')
+                    ->required()
+                    ->maxLength(255)
+                    ->default(1),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('customer_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('file_path')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPrescriptions::route('/'),
+            'create' => Pages\CreatePrescription::route('/create'),
+            'edit' => Pages\EditPrescription::route('/{record}/edit'),
+        ];
+    }
+}
