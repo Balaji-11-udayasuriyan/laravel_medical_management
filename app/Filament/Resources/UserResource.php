9a024cecc8847940e5cd988d71e6fa0b34873ebd
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-09-07 19:25:00 +0530

    Update UserResource.php

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -13,6 +13,8 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
+use App\Enums\Role;
+
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
