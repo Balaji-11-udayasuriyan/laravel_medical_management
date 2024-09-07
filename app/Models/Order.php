cccb9b304491adc5e3bf2fa532763259a69849c2
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-09-07 22:36:29 +0530

    create Order model and migration

---
+++ app/Models/Order.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    use HasFactory;
+
+    protected $fillable = [
+        
+        'user_id',
+        'total_price', 
+        'status'
+    
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
