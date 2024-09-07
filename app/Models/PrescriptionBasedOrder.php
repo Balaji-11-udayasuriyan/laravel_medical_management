0377649a1453ad2cc6275ee20add3ebd279aacc9
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-09-07 23:08:01 +0530

    create PrescriptionBasedOrder model and migration

---
+++ app/Models/PrescriptionBasedOrder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class PrescriptionBasedOrder extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        
+        'user_id', 
+        'order_id', 
+        'prescription_details'
+    
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class, 'order_id');
+    }
+}
---
+++ database/migrations/2024_09_07_173352_create_prescription_based_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prescription_based_orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('order_id');
+            $table->text('prescription_details');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prescription_based_orders');
+    }
+};
