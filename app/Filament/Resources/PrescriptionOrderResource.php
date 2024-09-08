5854fbea61b5aa12c2c12a58c9ec08b0746c17cb
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-09-08 21:55:23 +0530

    Group PrescriptionOrder, Prescription and PrescriptionReview resources under 'Prescription Management'  in laravel nova

--- app/Filament/Resources/PrescriptionOrderResource.php
+++ app/Filament/Resources/PrescriptionOrderResource.php
@@ -19,6 +19,8 @@ class PrescriptionOrderResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $navigationGroup = 'Prescription Management';
+
     public static function form(Form $form): Form
     {
         return $form
