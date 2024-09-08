c30f56450d471d7f6f54134ee1925d98198cf132
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-09-08 23:55:56 +0530

    Add role creation and AssignRoles for ( doctor, Pharamacist, Costomer)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,38 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@gmail.com',
             'password' => Hash::make('admin'),
         ]);
+        $admin->assignRole('Admin');
+
+        // Create a Doctor user
+        $doctor = User::create([
+            'name' => 'Doctor User',
+            'email' => 'doctor@example.com',
+            'password' => Hash::make('admin'),
+        ]);
+        $doctor->assignRole('Doctor');
+
+        // Create a Pharmacist user
+        $pharmacist = User::create([
+            'name' => 'Pharmacist User',
+            'email' => 'pharmacist@example.com',
+            'password' => Hash::make('admin'),
+        ]);
+        $pharmacist->assignRole('Pharmacist');
+
+        // Create a Customer user
+        $customer = User::create([
+            'name' => 'Customer User',
+            'email' => 'customer@example.com',
+            'password' => Hash::make('admin'),
+        ]);
+        $customer->assignRole('Customer');
+
+        //assign the roles
+
+        Role::create(['name' => 'Admin']);
+        Role::create(['name' => 'Doctor']);
+        Role::create(['name' => 'Pharmacist']);
+        Role::create(['name' => 'Customer']);
 
 
         $this->call(CategorySeeder::class);
