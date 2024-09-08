686b7e7a7c05587200f1e6cb8d5bd1ac10b319bc
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-09-08 12:06:14 +0530

    Update Appointment.php

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'doctor_id',
+        'user_id',
+        'appointment_time',
+        'status',
+        'consultation_type',
+        'notes',
+    ];
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
