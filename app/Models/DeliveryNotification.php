429c3d3809a34123bbb22ead4fb448c5bcba7a64
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-09-10 09:22:30 +0530

    Create DeliveryNotification model and migration

---
+++ app/Models/DeliveryNotification.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class DeliveryNotification extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+
+        'user_id',
+        'order_id',
+        'message',
+        'is_read',
+
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class, 'order_id');
+    }
+}
