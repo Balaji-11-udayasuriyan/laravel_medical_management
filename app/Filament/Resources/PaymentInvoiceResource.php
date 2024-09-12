dc5218f1cf496ff1adc46069cd000f0bb9b95b84
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-09-12 09:28:10 +0530

    create filament resource for PaymentInvoice model

---
+++ app/Filament/Resources/PaymentInvoiceResource.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PaymentInvoiceResource\Pages;
+use App\Filament\Resources\PaymentInvoiceResource\RelationManagers;
+use App\Models\PaymentInvoice;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PaymentInvoiceResource extends Resource
+{
+    protected static ?string $model = PaymentInvoice::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('order_id')
+                    ->relationship('order','name'),
+                Forms\Components\TextInput::make('invoice_number')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\DateTimePicker::make('invoice_date')
+                    ->required(),
+                Forms\Components\TextInput::make('total_amount')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\Select::make('status')
+                    ->options(PaymentInvoiceStatus::class),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('order_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('invoice_number')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('invoice_date')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('total_amount')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPaymentInvoices::route('/'),
+            'create' => Pages\CreatePaymentInvoice::route('/create'),
+            'edit' => Pages\EditPaymentInvoice::route('/{record}/edit'),
+        ];
+    }
+}
