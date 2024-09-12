85d331b418ab45eb8fa7d85c5c04c8686e6ec513
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-09-12 09:25:55 +0530

    update the word for PaymentInvoice migration

--- database/migrations/2024_09_12_034251_create_payment_invoices_table.php
+++ database/migrations/2024_09_12_034251_create_payment_invoices_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-use App\Enums\PaymentMethodStatus;
+use App\Enums\PaymentInvoiceStatus;
 
 return new class extends Migration
 {
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('invoice_number')->unique();
             $table->timestamp('invoice_date');
             $table->decimal('total_amount', 10, 2);
-            $table->string('status')->default(PaymentMethodStatus::Paid)->nullable();
+            $table->string('status')->default(PaymentInvoiceStatus::Paid)->nullable();
             $table->timestamps();
         });
     }
