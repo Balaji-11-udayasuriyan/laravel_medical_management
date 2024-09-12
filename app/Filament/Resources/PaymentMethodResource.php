90e9b4c14a330128150d91ef5a477bb97bf9a880
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-09-12 09:30:18 +0530

    Group PaymentInvoice, PaymentMethod, PaymentStatus and Payment under " Payment Management " in laravel nova

--- app/Filament/Resources/PaymentMethodResource.php
+++ app/Filament/Resources/PaymentMethodResource.php
@@ -19,6 +19,8 @@ class PaymentMethodResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $navigationGroup = 'Payment Management';
+
     public static function form(Form $form): Form
     {
         return $form
