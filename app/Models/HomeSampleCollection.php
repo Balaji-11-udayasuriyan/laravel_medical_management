8cef968af61f8f2333b7f1966cf4142c33710259
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-09-15 21:55:48 +0530

    Update HomeSampleCollection.php

--- app/Models/HomeSampleCollection.php
+++ app/Models/HomeSampleCollection.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
 class HomeSampleCollection extends Model
 {
     use HasFactory;
@@ -17,17 +20,17 @@ class HomeSampleCollection extends Model
         'scheduled_date',
         'scheduled_time',
         'status'
-        
+
     ];
 
     public function appointment()
     {
-        return $this->belongsTo(TestAppointment::class);
+        return $this->belongsTo(TestAppointment::class, 'appointment_id');
     }
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
     }
 
 }
