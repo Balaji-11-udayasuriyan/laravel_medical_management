c23247895ad746f804d98d8a04d52cfa4bedf82d
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-09-17 20:17:19 +0530

    Group DoctorReview, ProductReview and ServiceFeedback under " Review and Feedback Management " in laravel nova

--- app/Filament/Resources/ServiceFeedbackResource.php
+++ app/Filament/Resources/ServiceFeedbackResource.php
@@ -19,6 +19,8 @@ class ServiceFeedbackResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $navigationGroup = 'Review and Feedback Management';
+
     public static function form(Form $form): Form
     {
         return $form
