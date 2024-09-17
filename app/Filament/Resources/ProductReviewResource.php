3cc4ebbf74833a98259d9415df0cf29ee01e71e8
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-09-17 19:18:11 +0530

    create filament resource ProductReview model

---
+++ app/Filament/Resources/ProductReviewResource.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductReviewResource\Pages;
+use App\Filament\Resources\ProductReviewResource\RelationManagers;
+use App\Models\ProductReview;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProductReviewResource extends Resource
+{
+    protected static ?string $model = ProductReview::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('user_id')
+                    ->relationship('user','name'),
+                Forms\Components\Select::make('product_id')
+                    ->relationship('product','name'),
+                Forms\Components\TextInput::make('rating')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\Textarea::make('review')
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('user_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('product_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('rating')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProductReviews::route('/'),
+            'create' => Pages\CreateProductReview::route('/create'),
+            'edit' => Pages\EditProductReview::route('/{record}/edit'),
+        ];
+    }
+}
