0f48bbc0b46605948d32a0a354d3556b0fb4bda7
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-09-17 19:13:57 +0530

    Create ProductReview model and migration

---
+++ app/Models/ProductReview.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ProductReview extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+
+        'user_id',
+        'product_id',
+        'rating', // out of 5
+        'review',
+
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+}
