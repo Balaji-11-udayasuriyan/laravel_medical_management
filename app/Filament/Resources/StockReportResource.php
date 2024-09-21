52dc68b9822d96112ab8c364a8c1e6d061cb53f5
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-09-21 22:03:38 +0530

    Create filament resource for StockReport model

---
+++ app/Filament/Resources/StockReportResource.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\StockReportResource\Pages;
+use App\Filament\Resources\StockReportResource\RelationManagers;
+use App\Models\StockReport;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+use App\Enums\StockReportStatus;
+
+class StockReportResource extends Resource
+{
+    protected static ?string $model = StockReport::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('medicine_id')
+                    ->relationship('medicine','name'),
+                Forms\Components\TextInput::make('quantity')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('reorder_level')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\Select::make('status')
+                    ->options(StockReportStatus::class),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('medicine_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('quantity')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('reorder_level')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('status'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStockReports::route('/'),
+            'create' => Pages\CreateStockReport::route('/create'),
+            'edit' => Pages\EditStockReport::route('/{record}/edit'),
+        ];
+    }
+}
