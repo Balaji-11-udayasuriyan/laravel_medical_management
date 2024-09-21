c211add19267e9ef1f991304afe14336bbaa10c4
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-09-21 18:29:49 +0530

    Create filament resource for SaleReport model

---
+++ app/Filament/Resources/SaleReportResource.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SaleReportResource\Pages;
+use App\Filament\Resources\SaleReportResource\RelationManagers;
+use App\Models\SaleReport;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SaleReportResource extends Resource
+{
+    protected static ?string $model = SaleReport::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('order_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('amount')
+                    ->numeric()
+                    ->default(null),
+                Forms\Components\TextInput::make('payment_method')
+                    ->maxLength(255)
+                    ->default(null),
+                Forms\Components\TextInput::make('customer_type')
+                    ->maxLength(255)
+                    ->default(null),
+                Forms\Components\DatePicker::make('sale_date'),
+                Forms\Components\TextInput::make('location')
+                    ->maxLength(255)
+                    ->default(null),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('order_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('amount')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('payment_method')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('customer_type')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('sale_date')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('location')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSaleReports::route('/'),
+            'create' => Pages\CreateSaleReport::route('/create'),
+            'edit' => Pages\EditSaleReport::route('/{record}/edit'),
+        ];
+    }
+}
